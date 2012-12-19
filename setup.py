f5413f2c8fc5a73fe69cbab205dfe782b645165a
Michael Bauer <mihi@lo-res.org>
2012-12-19 20:41:48 +0100

    packaging...

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
-f=open("README.rst")
+f=open("README")
 setup(name='pycryptopan', version='0.01',
             py_modules=['cryptopan'],
             requires=['pycrypto'],
             url="https://github.com/FFM/pycryptopan",
             author="Michael Bauer",
-            email="mihi@lo-res.org",
+            author_email="mihi@lo-res.org",
             description="""A python implementation of Crypto-PAn 
               a ip anonymization algorithm""",
             long_description="\n".join(f)
