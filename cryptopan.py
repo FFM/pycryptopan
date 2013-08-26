694052ae7c296eff54711698468e394350415c60
Michael Bauer <mihi@lo-res.org>
2013-08-26 17:22:18 +0200

    added LICENSING information fixes #2
    Conflicts:
    	cryptopan.py

--- cryptopan.py
+++ cryptopan.py
@@ -1,4 +1,23 @@
+<<<<<<< HEAD
 from functools import reduce
+=======
+#   pycryptopan - a python module implementing the CryptoPAn algorithm
+#   Copyright (C) 2013 - the CONFINE project
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+>>>>>>> 35527bd... added LICENSING information fixes #12
 from Crypto.Cipher.AES import AESCipher as AES
 
 class CryptoPanError(Exception):
