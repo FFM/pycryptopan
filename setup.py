0db351ee02dfeba68f7a35a3d324bbcee4235964
aaronkaplan <aaron@lo-res.org>
2019-09-17 19:03:20 +0300

    redirect to me since mihi is deceased. Let's continue the work.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 f=open("README")
-setup(name='pycryptopan', version='0.01e',
+setup(name='pycryptopan', version='0.01f',
             py_modules=['cryptopan'],
             install_requires=['pycrypto'],                  
             requires=['pycrypto'],                  
-            url="https://github.com/FFM/pycryptopan",
-            author="Michael Bauer",
-            author_email="mihi@lo-res.org",
+            url="https://github.com/aaronkaplan/pycryptopan",
+            author="Aaron Kaplan",
+            author_email="aaron@lo-res.org",
             description="""A python implementation of Crypto-PAn 
-              a ip anonymization algorithm""",
+              a ip anonymization algorithm. Original first author: Michael Bauer (mihi@lo-res.org). 
+              Aaron Kaplan (second author, aaron@lo-res.org) is continuing the work when needed. 
+              Mihi R.I.P. Your code remembers you on the Internet.""",
             long_description="\n".join(f)
                   )
