e1ebbdf9f0e9f92e42833a9b4aa08b3a82baf836
aaronkaplan <aaron@lo-res.org>
2019-09-17 19:26:16 +0300

    format tests a bit nicer so that we can see better add a TODO file

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# Wishlist
+
+ - [ ] implement a `deanonymize(self, ip)` function
+ - [ ] include David Stott's AES-NI speedups
+ - [ ] compare correctness against other cryptoPan libs
+
+
+
--- test/test_pycryptopan.py
+++ test/test_pycryptopan.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env ipython3
+#!/usr/bin/env python3
 import unittest
 from cryptopan import CryptoPan
 
@@ -10,12 +10,15 @@ class TestPyCryptoPan(unittest.TestCase):
     c = CryptoPan(key)
 
     def test_good_encryption(self):
+        print("correctness test")
         self.assertEqual("206.2.124.120", self.c.anonymize("192.0.2.1"))
+        print("OK")
+        print("-" * 70)
 
     def test_perf(self):
         import time
         nb_tests = 1000
-        # print("starting performance check")
+        print("starting performance check")
         stime=time.time()
         for i in range(0, nb_tests):
             self.c.anonymize("192.0.2.1")
@@ -23,6 +26,7 @@ class TestPyCryptoPan(unittest.TestCase):
         print("%d anonymizations in %s s" %(nb_tests, dtime))
         print("rate: %f anonymizations /sec " %(nb_tests / dtime))
         self.assertTrue(True)
+        print("OK")
 
 
 if __name__ == '__main__':
