e458e8722ab6db1533e9acc3d6397c2ee16b4b31
aaronkaplan <aaron@lo-res.org>
2019-09-17 19:06:10 +0300

    format to pep8 standard.

--- test/test_pycryptopan.py
+++ test/test_pycryptopan.py
@@ -2,27 +2,28 @@
 import unittest
 from cryptopan import CryptoPan
 
+
 class TestPyCryptoPan(unittest.TestCase):
-  
-  # XXX FIXME! replace by YOUR key of course
-  key = "boojahyoo3vaeToong0Eijee7Ahz3yee"
-  c = CryptoPan(key)
 
-  def test_good_encryption(self):        
-    self.assertEqual("206.2.124.120",self.c.anonymize("192.0.2.1"))
+    # XXX FIXME! replace by YOUR key of course
+    key = "boojahyoo3vaeToong0Eijee7Ahz3yee"
+    c = CryptoPan(key)
 
-  def test_perf(self):
-    import time
-    nb_tests = 10
-    #print("starting performance check")
-    stime=time.time()
-    for i in range(0, nb_tests):
-        self.c.anonymize("192.0.2.1")
-    dtime=time.time() - stime
-    print("%d anonymizations in %s s" %(nb_tests, dtime))
-    print("rate: %f anonymizations /sec " %(nb_tests / dtime))
-    self.assertTrue(True)
+    def test_good_encryption(self):
+        self.assertEqual("206.2.124.120", self.c.anonymize("192.0.2.1"))
+
+    def test_perf(self):
+        import time
+        nb_tests = 1000
+        # print("starting performance check")
+        stime=time.time()
+        for i in range(0, nb_tests):
+            self.c.anonymize("192.0.2.1")
+        dtime=time.time() - stime
+        print("%d anonymizations in %s s" %(nb_tests, dtime))
+        print("rate: %f anonymizations /sec " %(nb_tests / dtime))
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
